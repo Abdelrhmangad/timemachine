c5729ad55bc63cf60d392b26a4e21e2292bd83d4
abdelrhmangad <abdelrhmangad114@gmail.com>
2024-06-26 12:40:42 +0300

    everything done but not fully

--- main.js
+++ main.js
@@ -1,76 +1,101 @@
-function showVisibleGroups() {
-    var a = timeline.getVisibleGroups();
-    document.getElementById("visibleGroupsContainer").innerHTML = "";
-    document.getElementById("visibleGroupsContainer").innerHTML += a;
+function formatDate(date) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    const dayName = days[date.getDay()];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    return `${dayName}, ${day}.${month}.${year}`;
 }
 
-var now = Date.now();
+function getHoursAndMinutes(date) {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
 
-var options = {
-    stack: true,
-    maxHeight: 640,
-    horizontalScroll: false,
-    verticalScroll: true,
-    zoomKey: "ctrlKey",
-    start: Date.now() - 1000 * 60 * 60 * 24 * 3, // minus 3 days
-    end: Date.now() + 1000 * 60 * 60 * 24 * 21, // plus 1 months aprox.
-    orientation: {
-        axis: "both",
-        item: "top",
+    return `${hours}:${minutes}`
+}
+
+
+var groups = new vis.DataSet([
+    { id: formatDate(new Date(2024, 5, 24, 8, 30, 0)), content: `${formatDate(new Date(2024, 5, 24, 8, 30, 0))}`, value: formatDate(new Date(2024, 5, 24, 8, 30, 0)) },
+    { id: formatDate(new Date(2024, 5, 23, 8, 30, 0)), content: `${formatDate(new Date(2024, 5, 23, 8, 30, 0))}`, value: formatDate(new Date(2024, 5, 23, 8, 30, 0)) },
+    { id: formatDate(new Date(2024, 5, 22, 8, 30, 0)), content: `${formatDate(new Date(2024, 5, 22, 8, 30, 0))}`, value: formatDate(new Date(2024, 5, 22, 8, 30, 0)) },
+    { id: formatDate(new Date(2024, 5, 21, 8, 30, 0)), content: `${formatDate(new Date(2024, 5, 21, 8, 30, 0))}`, value: formatDate(new Date(2024, 5, 21, 8, 30, 0)) },
+    { id: formatDate(new Date(2024, 5, 20, 8, 30, 0)), content: `${formatDate(new Date(2024, 5, 20, 8, 30, 0))}`, value: formatDate(new Date(2024, 5, 20, 8, 30, 0)) },
+]);
+
+// create a dataset with items
+// note that months are zero-based in the JavaScript Date object, so month 3 is April
+var items = new vis.DataSet([
+    {
+        id: 0,
+        group: formatDate(new Date(2024, 5, 24, 8, 30, 0)),
+        content: "item 0",
+        title: `${getHoursAndMinutes(new Date(2024, 5, 24, 8, 30, 0), true)} - ${getHoursAndMinutes(new Date(2024, 5, 24, 12, 30, 0), true)}`,
+        start: new Date(2024, 5, 24, 8, 30, 0),
+        end: new Date(2024, 5, 24, 12, 30, 0),
     },
-};
-var groups = new vis.DataSet();
-var items = new vis.DataSet();
+    {
+        id: 1,
+        group: formatDate(new Date(2024, 5, 23, 8, 30, 0)),
+        content: "item 1",
+        title: `${getHoursAndMinutes(new Date(2024, 5, 24, 8, 30, 0), true)} - ${getHoursAndMinutes(new Date(2024, 5, 24, 12, 30, 0), true)}`,
+        start: new Date(2024, 5, 23, 8, 30, 0),
+        end: new Date(2024, 5, 23, 12, 30, 0),
+    },
+    {
+        id: 2,
+        group: formatDate(new Date(2024, 5, 22, 8, 30, 0)),
+        content: "item 2",
+        title: `${getHoursAndMinutes(new Date(2024, 5, 24, 8, 30, 0), true)} - ${getHoursAndMinutes(new Date(2024, 5, 24, 12, 30, 0), true)}`,
+        start: new Date(2024, 5, 22, 8, 30, 0),
+        end: new Date(2024, 5, 22, 12, 30, 0),
+    },
+    {
+        id: 3,
+        group: formatDate(new Date(2024, 5, 21, 8, 30, 0)),
+        content: "item 3",
+        title: `${getHoursAndMinutes(new Date(2024, 5, 24, 8, 30, 0), true)} - ${getHoursAndMinutes(new Date(2024, 5, 24, 12, 30, 0), true)}`,
+        start: new Date(2024, 5, 21, 8, 30, 0),
+        end: new Date(2024, 5, 21, 12, 30, 0),
+    },
+    {
+        id: 4,
+        group: formatDate(new Date(2024, 5, 20, 8, 30, 0)),
+        content: "item 4",
+        title: `${getHoursAndMinutes(new Date(2024, 5, 24, 8, 30, 0), true)} - ${getHoursAndMinutes(new Date(2024, 5, 24, 12, 30, 0), true)}`,
+        start: new Date(2024, 5, 20, 8, 30, 0),
+        end: new Date(2024, 5, 20, 12, 30, 0),
+    }
+]);
 
-var count = 300;
-
-for (var i = 0; i < count; i++) {
-    var start = now + 1000 * 60 * 60 * 24 * (i + Math.floor(Math.random() * 7));
-    var end = start + 1000 * 60 * 60 * 24 * (1 + Math.floor(Math.random() * 5));
-
-    groups.add({
-        id: i,
-        content: "Task " + i,
-        order: i,
-    });
-
-    items.add({
-        id: i,
-        group: i,
-        start: start,
-        end: end,
-        type: "range",
-        content: "Item " + i,
-    });
-}
-
-// create a Timeline
+// create visualization
 var container = document.getElementById("visualization");
-timeline = new vis.Timeline(container, null, options);
+var options = {
+    groupOrder: function (a, b) {
+        return a.value - b.value;
+    },
+    showMinorLabels: false,
+    orientation: "bottom",
+    showWeekScale: true,
+    showTooltips: true,
+    selectable: false,
+    showCurrentTime: true,
+    timeAxis: { scale: 'hour', step: 1 },
+    horizontalScroll: false,
+    moveable: false,
+    editable: false,
+};
+
+var timeline = new vis.Timeline(container);
+var ONE_DAY_IN_MS = 1000 * 60 * 60;
+var now = new Date();
+var nowInMs = now.getTime();
+var oneDayFromNow = nowInMs + ONE_DAY_IN_MS;
+timeline.setWindow(nowInMs, oneDayFromNow);
+
+timeline.setOptions(options);
 timeline.setGroups(groups);
 timeline.setItems(items);
-
-function debounce(func, wait = 100) {
-    let timeout;
-    return function (...args) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            func.apply(this, args);
-        }, wait);
-    };
-}
-
-let groupFocus = (e) => {
-    let vGroups = timeline.getVisibleGroups();
-    let vItems = vGroups.reduce((res, groupId) => {
-        let group = timeline.itemSet.groups[groupId];
-        if (group.items) {
-            res = res.concat(Object.keys(group.items));
-        }
-        return res;
-    }, []);
-    timeline.focus(vItems);
-};
-this.timeline.on("scroll", debounce(groupFocus, 200));
-// Enabling the next line leads to a continuous since calling focus might scroll vertically even if it shouldn't
-// this.timeline.on("scrollSide", debounce(groupFocus, 200))
